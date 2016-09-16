35bd5cf4de44b4e3a737156fad0956107eeede9f
tuckyeah <h.rosebrock@gmail.com>
2016-09-16 12:52:22 -0400

    complete deck and card classes per demos in class

--- lib/card.rb
+++ lib/card.rb
@@ -1,14 +1,32 @@
 # A simple representation of a playing card.
 class Card
-  SUITS = %w(C D H S).freeze
+  include Comparable
+
+  def <=>(other)
+    suit_comparison = SUITS.index(suit) <=> SUITS.index(other.suit)
+    rank_comparison = RANKS.index(rank) <=> RANKS.index(other.rank)
+
+    # this compares the suits first because then it will group them when
+    # we make a deck - to just evaluate individual cards, we'd want to do
+    # rank_comparison.zero? then suit_comparison
+    # this means that unless they're the same suit, we'll be ordering by suit
+    if suit_comparison.zero?
+      rank_comparison
+    else
+      suit_comparison
+    end
+  end
+
+  SUITS = %w(C D H S).freeze # freeze makes it immutable
   RANKS = [(2..10).to_a, %w(J Q K A)].flatten.freeze
+  # flatten turns these two arrays into one
 
   attr_reader :suit, :rank
 
   def initialize(rank, suit)
-    raise ArgumentError,
+    raise ArgumentError, # guard clause
           "Suit: '#{suit}' not in #{SUITS}" unless SUITS.include? suit
-    raise ArgumentError,
+    raise ArgumentError, # guard clause
           "Rank: '#{rank}' not in #{RANKS}" unless RANKS.include? rank
 
     @suit = suit
--- lib/deck.rb
+++ lib/deck.rb
@@ -1,34 +1,54 @@
 require_relative 'card'
 
-# A simple represenation of a deck of playing cards
+# A simple representation of a deck of playing cards
 class Deck
-  attr_reader :deck
-  private :deck
+  attr_reader :storage # lets us access 'deck' inside the class
+  private :strorage # doesn't let us access 'deck' from outside the class
 
   def initialize
-    @deck = Card::SUITS.map do |suit|
+    # take all the card suits and all the card RANKS
+    # and create a card for each, then flatten that array.
+    @storage = Card::SUITS.map do |suit|
       Card::RANKS.map { |rank| Card.new(rank, suit) }
     end.flatten
   end
 
   # swap front and back somewhere in the middle third.
   def cut
-    count = deck.length
+    count = storage.length
     random = Random.rand(count / 3)
     cut_point = (count / 3 + random)
-    deck.replace deck.slice(cut_point, count - cut_point) +
-                 deck.slice(0, cut_point)
+    storage.replace storage.slice(cut_point, count - cut_point) +
+                                    storage.slice(0, cut_point)
+    self
   end
 
+  def count
+    storage.count
+  end
+
+  # takes the first card off the deck and returns a single card object
+  # this is an engineering fault because we can't chain other instance methods
+  # after draw
   def draw
-    @deck.shift
+    storage.shift
   end
 
+  # shuffles the deck and returns itself, otherwise we'd have an Array returned
+  # and Arrays don't have 'cut' or 'draw' as methods
+  # this returns Self so we can call other instance methods after shuffle
   def shuffle
-    @deck.shuffle!
+    storage.shuffle! # instance variable
     self
   end
 
   def deal(cards, *hands)
+    # deal the number of cards requested into the numbers of hands requested
+    # one at a time
+    cards.times do
+      hands.each do |hand|
+        hand << draw
+      end
+    end
   end
 end
